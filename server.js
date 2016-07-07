5870a7511d14a1c0a95d7ac837085cf9f39154c2
Benjamin E. Coe <bencoe@gmail.com>
2016-07-06 17:43:41 -0700

    feat: proxy now rewrites package JSON as it is served (#3)
    * feat: proxy now rewrites package JSON as it is served
    
    * docs: remove comment that immediately became stale
    
    * fix: whoops, we should populate the .travis.yml
    
    * fix: change variable name based on @nexdrew's code review

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express')
 
 var app = express()
 var request = require('request')
+var npmUrls = require('@npm/npm-urls')
 var url = require('url')
 
 function CouchUrlRewriteProxy (opts) {
@@ -16,17 +17,44 @@ function CouchUrlRewriteProxy (opts) {
 
     if (~['PUT', 'POST', 'DELETE'].indexOf(req.method)) payload.body = req.body
 
-    request(payload, function (err, response, body) {
+    var rewrite
+    if (
+      !req.path.match(/\/-\//) && // CouchDB API URLs.
+      !req.path.match(/\.tgz$/) && // tarball URLs.
+      req.method === 'GET' // we should only rewrite GET requests!
+    ) {
+      rewrite = true
+    } else {
+      rewrite = false
+    }
+
+    var r = request(payload, function (err, response, body) {
       var status = 500
-      if (response && response.status) status = response.status
+      if (response && response.statusCode) status = response.statusCode
       if (err) res.status(status).send(body)
+      else if (rewrite) {
+        rewriteUrls(res, status, body, opts.frontDoorHost)
+      }
     })
-    .pipe(res)
+
+    // only pipe if we're not performing rewrite.
+    if (!rewrite) r.pipe(res)
   }
 
   ['put', 'post', 'delete', 'get', 'head'].forEach(function (method) {
     app[method](/.*/, proxy)
   })
+}
+
+function rewriteUrls (res, status, body, frontDoorHost) {
+  try {
+    var json = JSON.parse(body)
+    npmUrls.rewriteOldTarballUrls(frontDoorHost, json)
+    res.status(status).send(JSON.stringify(json))
+  } catch (err) {
+    console.error(err.message)
+    res.status(status).send(body)
+  }
 }
 
 module.exports = function (opts, cb) {
--- test/couch-url-rewrite-proxy.js
+++ test/couch-url-rewrite-proxy.js
@@ -9,6 +9,7 @@ var targz = require('tar.gz')
 require('chai').should()
 
 console.info = function () {}
+console.error = function () {}
 
 describe('couch-url-rewrite-proxy', function () {
   var server = null
@@ -48,6 +49,42 @@ describe('couch-url-rewrite-proxy', function () {
       return done()
     })
     read.pipe(parse)
+  })
+
+  it('rewrites package JSON as it is served', function (done) {
+    var jsonPath = '/tiny-tarball'
+    var json = nock('http://www.example.com')
+      .get(jsonPath)
+      .reply(200, fs.readFileSync('./test/fixtures/tiny-tarball.json'))
+
+    request.get({
+      url: 'http://localhost:9999' + jsonPath,
+      json: true
+    }, function (err, res, body) {
+      if (err) return done(err)
+      json.done()
+      body.versions['1.0.0'].dist.tarball.should.equal(
+        'http://www.example.com/t/tiny-tarball/_attachments/tiny-tarball-1.0.0.tgz'
+      )
+      return done()
+    })
+  })
+
+  it('handles a malformed JSON response', function (done) {
+    var jsonPath = '/tiny-tarball'
+    var json = nock('http://www.example.com')
+      .get(jsonPath)
+      .reply(200, fs.readFileSync('./test/fixtures/tiny-tarball.tgz'))
+
+    request.get({
+      url: 'http://localhost:9999' + jsonPath
+    }, function (err, res, body) {
+      if (err) return done(err)
+      json.done()
+      res.statusCode.should.equal(200)
+      res.body.should.equal(fs.readFileSync('./test/fixtures/tiny-tarball.tgz', 'utf-8'))
+      return done()
+    })
   })
 
   after(function () {
